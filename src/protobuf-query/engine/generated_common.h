93e755cada121a35ca00f9aab862f22830e4258d
Sanchay Harneja <sanchay.h@gmail.com>
2017-06-05 08:15:16 +0530

    Refactor FROM macro to read multiple input files

--- src/protobuf-query/engine/generated_common.h
+++ src/protobuf-query/engine/generated_common.h
@@ -8,6 +8,7 @@ You may obtain the License at http://www.apache.org/licenses/LICENSE-2.0
 
 #pragma once
 
+#include <stdio.h>
 #include <fcntl.h>
 #include <algorithm>
 #include <iostream>
@@ -35,17 +36,28 @@ using uint16 = ::google::protobuf::uint16;
 using uint32 = ::google::protobuf::uint32;
 using uint64 = ::google::protobuf::uint64;
 
-void ParsePbFromFile(const string& file, google::protobuf::Message& proto) {
+void ReadFromZeroCopyInputStream(
+    google::protobuf::io::ZeroCopyInputStream& steam, string& data) {
+  const void* buffer;
+  int size;
+  while (steam.Next(&buffer, &size)) {
+    data.append((const char*) buffer, size);
+  }
+}
+
+void GetProtoFromFile(const string& file, google::protobuf::Message& proto) {
   int fd = open(file.c_str(), O_RDONLY);
   ASSERT(fd >= 0, "Unable to open file", file);
-  bool ret = false;
-  if (file.rfind(".gz") != string::npos) {
+  string data;
+  if ((file.size() > 3) && (file.substr(file.size()-3) == ".gz")) {
     google::protobuf::io::FileInputStream fis(fd);
     google::protobuf::io::GzipInputStream gzfis(&fis);
-    ret = proto.ParseFromZeroCopyStream(&gzfis);
+    ReadFromZeroCopyInputStream(gzfis, data);
   } else {
-    ret = proto.ParseFromFileDescriptor(fd);
+    google::protobuf::io::FileInputStream fis(fd);
+    ReadFromZeroCopyInputStream(fis, data);
   }
+  bool ret = proto.ParseFromString(data);
   close(fd);
   ASSERT(ret, "Unable to parse", file);
 }
@@ -53,8 +65,10 @@ void ParsePbFromFile(const string& file, google::protobuf::Message& proto) {
 #ifndef FROM
 #define FROM(argc, argv, protos) \
 ASSERT(argc > 1, "Proto binary file not provided");\
-protos.emplace_back();\
-ParsePbFromFile(argv[1], protos.back());
+for (int i=1; i<argc; i++) {\
+  protos.emplace_back();\
+  GetProtoFromFile(argv[i], protos.back());\
+}
 #endif
 
 template<typename T>
